9a981373abfc03b5acb09c2b72828d7fd4f803b8
Kevin Risqi <kevinrisqi22@gmail.com>
2020-04-23 12:00:17 +0700

    Add auth.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUm6k-TmJL2PrdEgzsKB3QdpSk5PvVHOU",
-    authDomain: "fir-login-7aa67.firebaseapp.com",
-    databaseURL: "https://fir-login-7aa67.firebaseio.com",
-    projectId: "fir-login-7aa67",
-    storageBucket: "fir-login-7aa67.appspot.com",
-    messagingSenderId: "1034079241165",
-    appId: "1:1034079241165:web:f74dc01e08f82209497a84"
+    apiKey: "AIzaSyBiRYazMwY20wZwcz_2__O0T9ndb-QBmh0",
+    authDomain: "reacts-js-dcd03.firebaseapp.com",
+    databaseURL: "https://reacts-js-dcd03.firebaseio.com",
+    projectId: "reacts-js-dcd03",
+    storageBucket: "reacts-js-dcd03.appspot.com",
+    messagingSenderId: "506204376723",
+    appId: "1:506204376723:web:851dcff1b530c87cca0766",
+    measurementId: "G-ZN945R31MV"
   };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
